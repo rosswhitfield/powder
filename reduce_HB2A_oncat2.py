88bbc15a2604f513532983e0a990ceb8940fd8a6
Ross Whitfield <whitfieldre@ornl.gov>
2018-09-14 17:21:39 -0400

    Update reduce_HB2A_oncat2.py

---
+++ reduce_HB2A_oncat2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+import os
+import sys
+import pyoncat
+import getpass
+import client
+try:
+    from postprocessing.publish_plot import publish_plot
+except ImportError:
+    from finddata import publish_plot
+
+sys.path.insert(0, "/opt/mantidnightly/bin")
+from mantid.simpleapi import HB2AReduce, SaveFocusedXYE, SavePlot1D
+
+filename = sys.argv[1]
+output_file = os.path.split(filename)[-1].replace('.dat', '.xye')
+outdir = sys.argv[2]
+
+ws = HB2AReduce(filename)
+SaveFocusedXYE(ws, Filename=os.path.join(outdir, output_file), SplitFiles=False, IncludeHeader=False)
+
+div = SavePlot1D(ws, OutputType='plotly')
+
+oncat = pyoncat.ONCat(
+    'https://oncat.ornl.gov',
+    client_id=client.ID,
+    client_secret=client.SECRET,
+    flow=pyoncat.CLIENT_CREDENTIALS_FLOW
+)
+
+oncat.login()
+
+filename = sys.argv[1]
+ipts = filename.split('/')[3]
+
+datafile = oncat.Datafile.retrieve(
+    filename,
+    facility="HFIR",
+    instrument="HB2A",
+    experiment=ipts,
+    projection=["indexed.run_number"],
+)
+
+try:
+    runNumber = datafile.to_dict()['indexed']['run_number']
+    request = publish_plot('HB2A', runNumber, files={'file': div}, config='/SNS/users/rwp/post_processing.conf')
+except KeyError:
+    print("This file doesn't have a run number")
