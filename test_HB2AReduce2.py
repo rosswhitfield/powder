6fbcbc8050c25f4677a11c3bab308f04f9a6784f
Ross Whitfield <whitfieldre@ornl.gov>
2020-11-13 16:19:23 -0500

    Update test_HB2AReduce2.py test_HB2AReduce_short2.py

---
+++ test_HB2AReduce2.py
@@ -0,0 +1,16 @@
+from mantid.simpleapi import HB2AReduce
+import glob
+
+failed=[]
+failed_msg = []
+
+for filename in glob.iglob('/HFIR/HB2A/IPTS-*/exp6*/Datafiles/HB2A_exp*_scan????.dat'):
+    try:
+        ws=HB2AReduce(filename, OutputDirectory='/tmp')
+    except Exception as e:
+        failed.append(filename)
+        failed_msg.append(e)
+
+for a, b in zip(failed, failed_msg):
+    print(a,b)
+print(len(failed))
